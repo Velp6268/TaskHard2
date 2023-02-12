2f9729a0e4a8d4d37b78c2efd86b10885e81fd96
Максим Ночевный <bagor6268@gmail.com>
2023-02-12 16:04:30 +0300

    Добавьте файлы проекта.

---
+++ TaskHard2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskHard2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сделаю чуть позже сегодня не успеваю
+        }
+    }
+}
